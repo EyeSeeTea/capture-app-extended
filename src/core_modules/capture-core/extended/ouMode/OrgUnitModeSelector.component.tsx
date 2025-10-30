2493325d54077c1c75371ad9f991ce4b5c3397f7
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-10-30 17:49:00 +0800

    update from class to function

--- src/core_modules/capture-core/extended/ouMode/OrgUnitModeSelector.component.tsx
+++ src/core_modules/capture-core/extended/ouMode/OrgUnitModeSelector.component.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import i18n from '@dhis2/d2-i18n';
+import { withStyles, WithStyles } from '@material-ui/core/styles';
 // @ts-expect-error - SelectorBarItem is available at runtime, but its TypeScript definition is not exposed by the UI library
 import { Menu, MenuItem, SelectorBarItem, spacers } from '@dhis2/ui';
-import { withStyles, type WithStyles } from '@material-ui/core/styles';
 import { ConditionalTooltip } from '../../components/Tooltips/ConditionalTooltip';
 
 const ouModeOptions = {
@@ -29,71 +29,55 @@ const styles = () => ({
     },
 });
 
-type OwnProps = {
-    onClickOuMode: (ouMode: string) => void;
-    selectedOuMode: string;
-    isReadOnly?: boolean;
-};
+type OrgUnitModeSelectorProps = {
+  onClickOuMode: (ouMode: string) => void;
+  selectedOuMode: string;
+  isReadOnly?: boolean;
+} & WithStyles<typeof styles>;
 
-type Props = OwnProps & WithStyles<typeof styles>;
+export const OrgUnitModeSelectorPlain: React.FC<OrgUnitModeSelectorProps> = ({
+    onClickOuMode,
+    selectedOuMode,
+    classes,
+    isReadOnly = false,
+}) => {
+    const [open, setOpen] = useState(false);
 
-type State = {
-    open: boolean;
-};
+    const handleClick = (selectedMode: string) => {
+        setOpen(false);
+        onClickOuMode(selectedMode);
+    };
 
-class OrgUnitModeSelectorPlain extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            open: false,
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(selectedOuMode: string) {
-        const { onClickOuMode } = this.props;
-        this.setState({ open: false });
-        onClickOuMode && onClickOuMode(selectedOuMode);
-    }
-
-    render() {
-        const { isReadOnly, classes, selectedOuMode } = this.props;
-
-        return (
-            <ConditionalTooltip
-                enabled={Boolean(isReadOnly)}
-                content={i18n.t('Choose an organisation unit before selecting the ouMode.')}
+    return (
+        <ConditionalTooltip
+            enabled={Boolean(isReadOnly)}
+            content={i18n.t('Choose an organisation unit before selecting the ouMode.')}
+        >
+            <SelectorBarItem
+                label={i18n.t('ouMode')}
+                noValueMessage={isReadOnly ? i18n.t('None selected') : i18n.t('Choose an organisation unit')}
+                value={ouModeOptions[selectedOuMode]?.label}
+                open={!isReadOnly && open}
+                setOpen={setOpen}
+                displayOnly={isReadOnly}
+                dataTest="org-unit-selector-container"
             >
-                <SelectorBarItem
-                    label={i18n.t('ouMode')}
-                    noValueMessage={isReadOnly ? i18n.t('None selected') : i18n.t('Choose an organisation unit')}
-                    value={ouModeOptions[selectedOuMode]?.label}
-                    open={!isReadOnly && this.state.open}
-                    setOpen={open => this.setState({ open })}
-                    displayOnly={isReadOnly}
-                    dataTest="org-unit-selector-container"
-                >
-                    <div className={classes.selectBarMenu}>
-                        <Menu>
-                            {
-                                Object.values(ouModeOptions).map(option => (
-                                    <MenuItem
-                                        key={option.value}
-                                        dense
-                                        onClick={() => this.handleClick(option.value)}
-                                        label={option.label}
-                                        suffix=""
-                                    />
-                                ))
-                            }
-                        </Menu>
-                    </div>
-                </SelectorBarItem>
-            </ConditionalTooltip>
-        );
-    }
-}
+                <div className={classes.selectBarMenu}>
+                    <Menu>
+                        {Object.values(ouModeOptions).map(option => (
+                            <MenuItem
+                                key={option.value}
+                                dense
+                                onClick={() => handleClick(option.value)}
+                                label={option.label}
+                                suffix=""
+                            />
+                        ))}
+                    </Menu>
+                </div>
+            </SelectorBarItem>
+        </ConditionalTooltip>
+    );
+};
 
 export const OrgUnitModeSelector = withStyles(styles)(OrgUnitModeSelectorPlain);
