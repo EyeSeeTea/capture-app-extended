0b95d741e18e78cc52aa1745e573ed4280894f52
Miquel Adell <miquel.adell@gmail.com>
2025-11-07 13:47:31 +0100

    [chore] change app name

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,6 @@
 const config = {
-    name: 'capture',
-    title: 'Capture',
+    name: 'capture-extended',
+    title: 'Capture Extended',
     type: 'app',
 
     id: '92b75fd0-34cc-451c-942f-3dd0f283bcbd',
